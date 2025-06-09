3992d7dd362b054c25557275b095f3952a856286
Hutomo Sakti Kartiko <hutomosaktikartiko.dev@gmail.com>
2025-06-09 21:16:03 +0700

    feat: storage basics

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,12 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
 
+	#[pallet::storage]
+	pub(super) type CountForKitties<T: Config> = StorageValue<Value = u32, QueryKind = ValueQuery>;
+
+	#[pallet::storage]
+	pub(super) type Kitties<T: Config> = StorageMap<Key = [u8; 32], Value = ()>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
@@ -25,13 +31,20 @@ pub mod pallet {
 	}
 
 	#[pallet::error]
-	pub enum Error<T> {}
+	pub enum Error<T> {
+		/// This is a description for the error.
+		///
+		/// This description can be shown to ther user in UIs, so make it descriptive.
+		TooManyKitties,
+		DuplicateKitty,
+	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		pub fn create_kitty(origin: OriginFor<T>) -> DispatchResult {
 			let who = ensure_signed(origin)?;
-			Self::mint(who)?;
+			let dna = [0u8; 32];
+			Self::mint(who, dna)?;
 			Ok(())
 		}
 	}
